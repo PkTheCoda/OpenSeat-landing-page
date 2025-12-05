531ead93b0fbd62c60b7ac61600db0fa33841a01
PkTheCoda <pranavkonjeti@gmail.com>
2025-12-05 15:52:47 -0600

    QOL changes

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
       >
         <div className="py-4 w-full px-10">
           <nav class=" py-2.5 dark:bg-gray-900">
-            <div class="flex flex-wrap items-center justify-between w-full px-4 mx-auto">
+            <div class="flex flex-wrap items-center justify-between w-full px-4 mx-auto max-w-[60rem]">
               <a href="#" class="flex items-center">
                 <img src={openseatlogo} alt="" className="w-40"/>
               </a>
@@ -71,11 +71,11 @@ const Hero = () => {
                 class="items-center justify-between w-full lg:flex lg:w-auto lg:order-1"
                 id="mobile-menu-2"
               >
-                <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0 items-center">
                   <li>
                     <a
                       href="#"
-                      class="block py-2 pl-3 pr-4 text-white bg-purple-700 rounded lg:bg-transparent lg:text-purple-700 lg:p-0 dark:text-white"
+                      class="block py-1 px-2 text-white rounded lg:text-orange-300 bg-orange-50/10  dark:text-white"
                       aria-current="page"
                     >
                       Home
@@ -84,7 +84,7 @@ const Hero = () => {
                   <li>
                     <a
                       href="#"
-                      class="block py-2 pl-3 pr-4 text-slate-100 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                      class="block py-2 pl-3 pr-4 text-slate-100 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-orange-300 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                     >
                       About
                     </a>
@@ -92,7 +92,7 @@ const Hero = () => {
                   <li>
                     <a
                       href="#"
-                      class="block py-2 pl-3 pr-4 text-slate-100 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                      class="block py-2 pl-3 pr-4 text-slate-100 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-orange-300 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                     >
                       Features
                     </a>
@@ -100,7 +100,7 @@ const Hero = () => {
                   <li>
                     <a
                       href="#"
-                      class="block py-2 pl-3 pr-4 text-slate-100 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                      class="block py-2 pl-3 pr-4 text-slate-100 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-orange-300 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                     >
                       Contact
                     </a>
@@ -113,7 +113,7 @@ const Hero = () => {
 
         <div className="flex flex-col items-center justify-center gap-y-4 max-w-[90rem] mx-auto w-full py-10 px-2">
           <h1 className="eight-title text-center font-medium tracking-tighter text-slate-100">
-            Find Your Seat
+            Find Your Seat,
             <span className="landing-text-gradient opacity-80">
               <br /> Skip The Search.
             </span>
